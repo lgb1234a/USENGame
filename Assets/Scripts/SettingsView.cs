2e9b278178c36cb20b5399b626ee0e9ce1b98570
chenyn <chenynle@gmail.com>
2023-04-17 21:32:36 +0800

    feat: slider

--- Assets/Scripts/SettingsView.cs
+++ Assets/Scripts/SettingsView.cs
@@ -6,6 +6,8 @@ public class SettingsView : IViewOperater
 {
     string m_prefabPath = "SettingsPanel";
     GameObject m_viewGameObject;
+    Slider m_BGMSlider;
+    Text m_BGMVolumeText;
 
     public SettingsView() {
         var obj = Resources.Load<GameObject>(m_prefabPath);
@@ -13,6 +15,11 @@ public class SettingsView : IViewOperater
         var position = m_viewGameObject.transform.localPosition;
         position.z = 0;
         m_viewGameObject.transform.localPosition = position;
+
+        m_BGMSlider = m_viewGameObject.transform.Find("Panel/CenterPanel/BGMSettings/Slider").GetComponent<Slider>();
+        m_BGMSlider.onValueChanged.AddListener(OnBGMSliderValueChanged);
+        m_BGMVolumeText = m_viewGameObject.transform.Find("Panel/CenterPanel/BGMSettings/VolumeText").GetComponent<Text>();
+        EventSystem.current.SetSelectedGameObject(m_BGMSlider.gameObject);
     }
 
 
@@ -37,5 +44,9 @@ public class SettingsView : IViewOperater
 
     public void OnClickBackHomeButton() {
         Hide();
+    }
+
+    public void OnBGMSliderValueChanged(float value) {
+        m_BGMVolumeText.text = Mathf.FloorToInt(value).ToString();
     }
 }
