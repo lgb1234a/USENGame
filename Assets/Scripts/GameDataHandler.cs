d10602547536daca5bf92ea070352f24861b81cd
chenyn <chenynle@gmail.com>
2023-09-12 21:14:47 +0800

    转数字随机策略调整

--- Assets/Scripts/GameDataHandler.cs
+++ Assets/Scripts/GameDataHandler.cs
@@ -34,6 +34,28 @@ public class GameDataHandler
         }
     }
 
+    private int lastUnchecked = 0;
+
+    private bool IsRestLastFiveCells()
+    {
+        return m_checkedNumbers.Count >= m_cellCount - 5;
+    }
+
+    private int[] GetRestUncheckedCellNums()
+    {
+        var nums = new List<int>();
+        for(int i = 0; i < m_cellCheckedList.Count; i++)
+        {
+            if (m_cellCheckedList[i] == false)
+            {
+                nums.Add(i + 1);
+            }
+        }
+        return nums.ToArray();
+    }
+
+    private int lastUncheckTextIndex = 0;
+
     public int GetUncheckedNumber() {
         var validNumbers = new List<int>();
         for (int i = 0; i < m_cellCount; i++)
@@ -43,7 +65,26 @@ public class GameDataHandler
         var index = UnityEngine.Random.Range(0, validNumbers.Count-1);
         if (index >= validNumbers.Count)
             return 0;
-        return validNumbers[index];
+
+        var number = validNumbers[index];
+        if (IsRestLastFiveCells())
+        {
+            var uncheckedList = GetRestUncheckedCellNums();
+            return uncheckedList[lastUncheckTextIndex++ % uncheckedList.Length];
+        }
+        else 
+        {
+            if (number == lastUnchecked)
+            {
+                return GetUncheckedNumber();
+            }
+            else
+            {
+                lastUnchecked = number;
+            }
+        }
+        
+        return number;
     }
 
     public bool IsAllChecked() {
