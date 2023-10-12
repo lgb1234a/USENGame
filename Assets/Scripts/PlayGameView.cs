7399001ffb9be3afb060cc3508d4273c094a9a82
chenyn <chenynle@gmail.com>
2023-10-12 21:35:02 +0800

    reach和bingo次数优化

--- Assets/Scripts/PlayGameView.cs
+++ Assets/Scripts/PlayGameView.cs
@@ -225,6 +225,7 @@ public class PlayGameView : IViewOperater
     public void Hide() {
         m_viewGameObject.SetActive(false);
         m_pausePanel.gameObject.SetActive(false);
+        AudioManager.Instance.StopNumberRotateEffect();
     }
 
     IEnumerable<Transform> GenerateNumberCells() {
@@ -432,14 +433,16 @@ public class PlayGameView : IViewOperater
         if (m_checkAnimator.isAnimating()) return;
         if (!m_canPlayBingoAnim) return;
         AudioManager.Instance.PlayBingoEffect();
+
+        if (m_reachCount != 0) {
+            m_bingoCount++;
+        }
         
         if (m_bingoCount == m_reachCount) {
             AudioManager.Instance.PlayDefaultBgm(1f);
             m_bgEffect.AnimationState.SetAnimation(0, "panel_blue", true);
-        }else
-        {
-            m_bingoCount++;
         }
+        
         m_sg.transform.parent.gameObject.SetActive(true);
         m_sg.AnimationState.SetAnimation(0, "bingo", false);
 
