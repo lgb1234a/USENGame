84bca3878623229634f0e2d1125aa43c9e501795
chenyn <chenynle@gmail.com>
2023-11-18 21:43:01 +0800

    显示debug信息

--- Assets/Scripts/ViewManager.cs
+++ Assets/Scripts/ViewManager.cs
@@ -123,6 +123,7 @@ public class ViewManager : MonoBehaviourSingletonTemplate<ViewManager>
     }
 
     public void OnAndroidKeyDown(string keyName) {
+        ShowDebugInfo($"Android key:{keyName}");
         // Debug.Log($"Call from android key:{keyName}");
         m_currentView.OnAndroidKeyDown(keyName);
 
