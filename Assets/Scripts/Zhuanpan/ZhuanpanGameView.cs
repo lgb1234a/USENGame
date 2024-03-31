a6bc8fb07d300eead65cfd9d63f1b03118304b42
chenyn <chenynle@gmail.com>
2024-03-31 20:32:18 +0800

    feat: 转盘游戏页面划分

---
+++ Assets/Scripts/Zhuanpan/ZhuanpanGameView.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class ZhuanpanGameView : AbstractView, IViewOperater
+{
+    string m_prefabPath = "ZhuanpanGamePanel";
+    HighAndLowFAQView m_faqView;
+    HighAndLowSettingsView m_settingsView;
+    public void Build()
+    {
+        m_mainViewGameObject = LoadViewGameObject(m_prefabPath, ViewManager.Instance.GetRootTransform());
+
+    }
+
+    public void Hide()
+    {
+        m_mainViewGameObject.SetActive(false);
+    }
+
+    public void OnAndroidKeyDown(string keyName)
+    {
+        if (keyName == "blue")
+        {
+            if (m_faqView == null) {
+                m_faqView = new HighAndLowFAQView();
+            }
+            ViewManager.Instance.Push(m_faqView);
+        }
+
+        if (keyName == "red")
+        {
+            if (m_settingsView == null) {
+                m_settingsView = new HighAndLowSettingsView();
+            }
+            ViewManager.Instance.Push(m_settingsView);
+        }
+    }
+
+    public void OnThemeTypeChanged()
+    {
+        
+    }
+
+    public void Show()
+    {
+        m_mainViewGameObject.SetActive(true);
+        // EventSystem.current.SetSelectedGameObject(Btn1.gameObject);
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Cancel")) {
+            USENSceneManager.Instance.LoadScene("GameEntries");
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            //测试
+            if (m_faqView == null) {
+                m_faqView = new HighAndLowFAQView();
+            }
+            ViewManager.Instance.Push(m_faqView);
+        }
+
+        if (Input.GetKeyDown(KeyCode.B)) {
+            //测试
+            if (m_settingsView == null) {
+                m_settingsView = new HighAndLowSettingsView();
+            }
+            ViewManager.Instance.Push(m_settingsView);
+        }
+    }
+}
---
+++ Assets/Scripts/Zhuanpan/ZhuanpanQuitView.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Spine.Unity;
+
+public class ZhuanpanQuitView : AbstractView, IViewOperater
+{
+    string m_prefabPath = "ZhuanpanQuitPanel";
+    Button Btn1;
+    Button Btn2;
+    HighAndLowFAQView m_faqView;
+    HightAndLowGameView m_gameView;
+    HighAndLowSettingsView m_settingsView;
+    public void Build()
+    {
+        m_mainViewGameObject = LoadViewGameObject(m_prefabPath, ViewManager.Instance.GetRootTransform());
+
+        Btn1 = m_mainViewGameObject.transform.Find("Btn1").GetComponent<Button>();
+        Btn1.onClick.AddListener(OnClickedBtn1);
+
+        Btn2 = m_mainViewGameObject.transform.Find("Btn2").GetComponent<Button>();
+        Btn2.onClick.AddListener(OnClickedBtn2);
+    }
+
+    public void Hide()
+    {
+        m_mainViewGameObject.SetActive(false);
+    }
+
+    public void OnAndroidKeyDown(string keyName)
+    {
+        if (keyName == "blue")
+        {
+            if (m_faqView == null) {
+                m_faqView = new HighAndLowFAQView();
+            }
+            ViewManager.Instance.Push(m_faqView);
+        }
+
+        if (keyName == "yellow")
+        {
+            if (m_settingsView == null) {
+                m_settingsView = new HighAndLowSettingsView();
+            }
+            ViewManager.Instance.Push(m_settingsView);
+        }
+    }
+
+    public void OnThemeTypeChanged()
+    {
+        
+    }
+
+    public void Show()
+    {
+        m_mainViewGameObject.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(Btn1.gameObject);
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Cancel")) {
+            USENSceneManager.Instance.LoadScene("GameEntries");
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            //测试
+            if (m_faqView == null) {
+                m_faqView = new HighAndLowFAQView();
+            }
+            ViewManager.Instance.Push(m_faqView);
+        }
+
+        if (Input.GetKeyDown(KeyCode.B)) {
+            //测试
+            if (m_settingsView == null) {
+                m_settingsView = new HighAndLowSettingsView();
+            }
+            ViewManager.Instance.Push(m_settingsView);
+        }
+    }
+
+    public void OnClickedBtn1()
+    {
+        if (m_gameView == null) {
+            m_gameView = new HightAndLowGameView();
+        }
+        ViewManager.Instance.Push(m_gameView);
+    }
+
+    public void OnClickedBtn2()
+    {
+        if (m_gameView == null) {
+            m_gameView = new HightAndLowGameView();
+        }
+        ViewManager.Instance.Push(m_gameView);
+    }
+}
