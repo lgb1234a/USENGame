9a5b582e2818625de45d83da186c628351a9c775
LunarEclipse <949886@qq.com>
2024-09-02 14:18:18 +0800

    山手线问题列表环绕效果

--- Assets/Games/Yamanote/UIs/YamanoteCategoryList.cs
+++ Assets/Games/Yamanote/UIs/YamanoteCategoryList.cs
@@ -8,6 +8,14 @@ namespace USEN.Games.Yamanote
     
     public class YamanoteCategoryList : FixedListView<YamanoteCategoryListCell, YamanoteCategory>, IEventSystemHandler
     {
+        public RectTransform ring;
+
+        private Vector2 RingCenter => ring.anchoredPosition - RectTransform.anchoredPosition;
+        private float RingRadius => ring.rect.width / 2;
+        
+        private RectTransform RectTransform => (RectTransform)transform;
+        
+        
         protected void Start()
         {
             var content = _scrollRect.content;
@@ -33,6 +41,26 @@ namespace USEN.Games.Yamanote
                         "日本の車メーカー",
                     },
                 }; 
+            }
+        }
+        
+        private void Update()
+        {
+            // Pin every cell to the circumference of the ring
+            UpdateCellPositions();
+        }
+
+        private void UpdateCellPositions()
+        {
+            for (int i = 0; i < cells.Count; i++)
+            {
+                var cell = cells[i];
+                var cellTransform = (RectTransform)cell.transform;
+                var y = cellTransform.anchoredPosition.y;
+                var angle = Mathf.Asin((cellTransform.anchoredPosition.y + RectTransform.rect.height * 0.5f - RingCenter.y + _scrollRect.content.anchoredPosition.y) / RingRadius);
+                var x = RingRadius * Mathf.Cos(angle) - 50f;
+                var position = new Vector2(x, y);
+                cellTransform.anchoredPosition = position;
             }
         }
 
--- Assets/Games/Yamanote/UIs/YamanoteQuestionsList.cs
+++ Assets/Games/Yamanote/UIs/YamanoteQuestionsList.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections;
+using Cysharp.Threading.Tasks;
+using Luna.Extensions.Unity;
 using Luna.UI;
 using Luna.UI.Navigation;
 using UnityEngine;
@@ -8,6 +12,34 @@ namespace USEN.Games.Yamanote
     
     public class YamanoteQuestionsList : FixedListView<YamanoteQuestionsListCell, string>, IEventSystemHandler
     {
+        public RectTransform ring;
+
+        private Vector2 RingCenter => ring.anchoredPosition - RectTransform.anchoredPosition;
+        private float RingRadius => ring.rect.width / 2;
+        
+        private RectTransform RectTransform => (RectTransform)transform;
+
+        
+        private void Update()
+        {
+            // Pin every cell to the circumference of the ring
+            UpdateCellPositions();
+        }
+
+        private void UpdateCellPositions()
+        {
+            for (int i = 0; i < cells.Count; i++)
+            {
+                var cell = cells[i];
+                var cellTransform = (RectTransform)cell.transform;
+                var y = cellTransform.anchoredPosition.y;
+                var angle = Mathf.Asin((cellTransform.anchoredPosition.y + RectTransform.rect.height * 0.5f - RingCenter.y + _scrollRect.content.anchoredPosition.y) / RingRadius);
+                var x = RingRadius * Mathf.Cos(angle) - 50f;
+                var position = new Vector2(x, y);
+                cellTransform.anchoredPosition = position;
+            }
+        }
+
         protected override void OnCellClicked(int index, YamanoteQuestionsListCell listViewCell)
         {
             Navigator.Push<YamanoteGameView>((view) =>
